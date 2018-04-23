687ba28aa94cf9a6f0dd5b309acab8837663d57c
lutangar <johan.dufour@gmail.com>
2018-04-23 11:51:11 +0200

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # observable-child-process
-Spawn observable child processes
+Spawn observable child processes.
+
+## Usage
+For example to build and push a docker image:
+```
+const { spawn } = require('observable-child-process');
+
+spawn('docker', ['build', '-t', 'myimage'], { env: { ...process.env, MY_CUSTOM_ENV_VARIABLE: 'foo' })
+.concat(spawn('docker', ['push', 'myimage']));
+```
+
+## Resources
+* https://nodejs.org/api/child_process.html
+* http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html
+
+## Licence
+MIT
---
+++ index.js
@@ -0,0 +1,31 @@
+const { Observable } = require('rxjs/Observable');
+const childProcess = require('child_process');
+const readline = require('readline');
+const chalk = require('chalk');
+
+const spawn = (command, args = [], options = {}) => Observable.create((observer) => {
+    const spawnee = childProcess.spawn(command, args, options);
+    console.log(chalk`{white.bold ${command} ${args.join(' ')}}`);
+
+    readline
+        .createInterface({ input: spawnee.stdout, terminal: false })
+        .on('line', line => console.log(chalk`{white ${line}}`))
+    ;
+
+    readline
+        .createInterface({ input: spawnee.stderr, terminal: false })
+        .on('line', line => console.error(chalk`{red ${line}}`))
+    ;
+
+    spawnee.on('close', (code) => {
+        if (code === 0) {
+            observer.complete();
+        } else {
+            observer.error(new Error(`child process exited with code ${code}`));
+        }
+    });
+});
+
+module.exports = {
+    spawn,
+};
